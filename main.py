3eea3b84cfb49b459dd71bb89b1e2ce2fdf23bc0
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-03-06 23:55:17 +0100

    finished interface

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from Object.deck import Deck
 from service import Service
+import os
 
 class main:
     def startGame(self):
+        os.system('cls')
         deckPlayerA = Deck().getCardsToDeck()
         handPlayerA = Deck().getHand(deckPlayerA)
         deckPlayerB = Deck().getCardsToDeck()
         handPlayerB = Deck().getHand(deckPlayerB)
-        print('Witaj w grze karcianej')
-        print('\nGracz A')
-        choosePlayerA = Service().choseOfPlayer(handPlayerA)
-        print('\nGracz B')
-        choosePlayerB = Service().choseOfPlayer(handPlayerB)
-        Service().cheeck(choosePlayerA, choosePlayerB)
+        service = Service(handPlayerA, handPlayerB)
+        print('Welcome in my card game')
+        while True:
+            service.resoults()
+            if service.resoults() == False:
+                break
+            print('\nPlayer A')
+            choosePlayerA = service.choseOfPlayerA()
+            print('\nPlayer B')
+            choosePlayerB = service.choseOfPlayerB()
+            service.cheeck(choosePlayerA, choosePlayerB)
+            while True:
+                if input('To continue click ENTER')=='':
+                    service.addCard(deckPlayerA, deckPlayerB)
+                    os.system('cls')
+                    break
 
 main().startGame()
