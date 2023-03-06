3fda277d4d2578cb4ba258d4e4da6f2f9c600e36
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-03-06 23:56:57 +0100

    add resoults(which player won), addCard(after every round), finished cheech(main logic who win trade etc.)

--- service.py
+++ service.py
@@ -1,16 +1,49 @@
 from Object.deck import Deck
-from Object.cards import Card
+import random
 
 class Service:
-    def choseOfPlayer(self, hand):
-        Deck().showHand(hand)
-        choose=int(input('Wybierz karte z talii '))-1
-        return hand[choose]
+    def __init__(self,handA, handB):
+        self.handA = handA
+        self.handB = handB
+    def choseOfPlayerA(self):
+        Deck().showHand(self.handA)
+        choose = int(input('Chose card from your hand '))-1
+        card = self.handA[choose]
+        self.handA.pop(choose)
+        return card
+    def choseOfPlayerB(self):
+        Deck().showHand(self.handB)
+        choose = int(input('Chose card from your hand '))-1
+        card = self.handB[choose]
+        self.handB.pop(choose)
+        return card
 
     def cheeck(self,choseA, choseB):
         if choseA.getCost > choseB.getCost:
-            print('Wygral gracz A')
+            self.handA.append(choseA)
+            self.handA.append(choseB)
+            print('\nPlayer A win this brawl!!')
         elif choseB.getCost > choseA.getCost:
-            print('Wygral gracz B')
+            self.handB.append(choseA)
+            self.handB.append(choseB)
+            print('\nPlayer B win this brawl!!')
         else:
-            print('Remis')
+            self.handA.append(choseA)
+            self.handB.append(choseB)
+            print('\nRemis')
+
+    def addCard(self, deckA, deckB):
+        randomIntA = random.randint(0, len(deckA) - 1)
+        randomIntB = random.randint(0, len(deckB) - 1)
+        self.handA.append(deckA[randomIntA])
+        self.handB.append(deckA[randomIntB])
+
+    def resoults(self):
+        if len(self.handA)==0:
+            print("Player B won")
+            return False
+        elif len(self.handB)==0:
+            print("Player A won")
+            return False
+        else:
+            pass
